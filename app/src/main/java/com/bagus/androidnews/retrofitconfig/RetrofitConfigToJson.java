6f705ed9ed72d6e1e6a6881d1d141a6325b78928
BagusSatria123 <satriabagusputra123@gmail.com>
2020-07-28 10:33:35 +0700

    membuat interface GetJsonAll

--- app/src/main/java/com/bagus/androidnews/retrofitconfig/RetrofitConfigToJson.java
+++ app/src/main/java/com/bagus/androidnews/retrofitconfig/RetrofitConfigToJson.java
@@ -3,4 +3,9 @@ package com.bagus.androidnews.retrofitconfig;
 public class RetrofitConfigToJson {
 
     public static String URL_API = "https://newsapi.org/";
+
+    public static GetJsonAll getResponse(){
+
+        return RetrofitInstance.getInstance(URL_API).create(GetJsonAll.class);
+    }
 }
