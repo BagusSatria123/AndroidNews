a38a8f73044d1075202908d8b321e330a21c577f
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-04 08:54:40 +0700

    design activity_main.xml dengan recycle view dan menyambungkan MainActivity.java dengan MyAdapter agar data json bisa terlampir di activity_main.xml

--- app/src/main/java/com/bagus/androidnews/MainActivity.java
+++ app/src/main/java/com/bagus/androidnews/MainActivity.java
@@ -1,10 +1,13 @@
 package com.bagus.androidnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bagus.androidnews.adapter.MyAdapter;
 import com.bagus.androidnews.retrofitconfig.GetJsonAll;
 import com.bagus.androidnews.retrofitconfig.RetrofitConfigToJson;
 import com.bagus.androidnews.retrofitjson.News;
@@ -22,13 +25,24 @@ public class MainActivity extends AppCompatActivity {
     List<News> newsList;
     GetJsonAll getJsonAll;
     String title,description;
+
+    RecyclerView recycler_view;
+    MyAdapter myAdapter;
+    GridLayoutManager gm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recycler_view = findViewById(R.id.recycler_view);
+        gm = new GridLayoutManager(this,1);
+        recycler_view.setLayoutManager(gm);
+        myAdapter = new MyAdapter(this,newsList);
+        recycler_view.setAdapter(myAdapter);
+
         newsList = new ArrayList<>();
         getJsonAll = RetrofitConfigToJson.getResponse();
+
 
         getJsonAll.getNewsList("id","4e9e26cdfe464bb29a42c681c2c5089b").enqueue(new Callback<NewsList>() {
             @Override
@@ -40,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
                 title = newsList.get(1).getTitle();
                 description = newsList.get(1).getDescription();
                 Log.d("titleBerita","Judul " + title + " " + "Deskriptis " + description);
+
+                myAdapter = new MyAdapter(getApplicationContext(),newsList);
+                recycler_view.setAdapter(myAdapter);
+                myAdapter.notifyDataSetChanged();
             }
 
             @Override
