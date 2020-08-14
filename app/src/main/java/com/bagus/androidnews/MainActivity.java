de99cbb2cfd09a1085aed446d7df9807866249cc
BagusSatria123 <satriabagusputra123@gmail.com>
2020-08-14 21:13:37 +0700

    membuat bottom navigation

--- app/src/main/java/com/bagus/androidnews/MainActivity.java
+++ app/src/main/java/com/bagus/androidnews/MainActivity.java
@@ -1,17 +1,20 @@
 package com.bagus.androidnews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.bagus.androidnews.adapter.MyAdapter;
 import com.bagus.androidnews.retrofitconfig.GetJsonAll;
 import com.bagus.androidnews.retrofitconfig.RetrofitConfigToJson;
 import com.bagus.androidnews.retrofitjson.News;
 import com.bagus.androidnews.retrofitjson.NewsList;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +23,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     List<News> newsList;
     GetJsonAll getJsonAll;
@@ -29,12 +32,16 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recycler_view;
     MyAdapter myAdapter;
     GridLayoutManager gm;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recycler_view = findViewById(R.id.recycler_view);
+
+        bottomNavigationView = findViewById(R.id.bottom);
+
+        /*recycler_view = findViewById(R.id.recycler_view);
         gm = new GridLayoutManager(this,1);
         recycler_view.setLayoutManager(gm);
         newsList = new ArrayList<>();
@@ -66,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d("gagal",t + "");
             }
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
